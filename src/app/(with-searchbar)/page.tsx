87a706cccacdce2e801c3e23eaed636dd23f0412
Min-byeonghyun <qudgus5125@naver.com>
2025-08-26 17:33:39 +0900

    navigating

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,5 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      INDEX 페이지
-    </div>
-  );
+  return <div className={styles.page}>INDEX 페이지</div>;
 }
