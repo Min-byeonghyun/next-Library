4b40dd421fc95ea869a40818090f762e642304df
Min-byeonghyun <qudgus5125@naver.com>
2025-08-27 15:40:49 +0900

    data cache

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -22,7 +22,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { cache: "force-cache" }
   );
   if (!response.ok) {
     return <div>추천 도서를 불러올 수 없습니다.</div>;
