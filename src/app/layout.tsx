9bf748277d2b28de6bd8935150a072708472b206
Min-byeonghyun <qudgus5125@naver.com>
2025-08-30 19:44:18 +0900

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body>
         <div className={style.container}>
           <header>
