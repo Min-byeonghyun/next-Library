845013bbc400e817ade2af5aca833b3284be3f35
Min-byeonghyun <qudgus5125@naver.com>
2025-09-18 14:41:42 +0900

    next image 최적화

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -3,7 +3,7 @@ import style from "./page.module.css";
 import { ReviewData } from "@/types";
 import ReviewItem from "@/components/review-item";
 import ReviewEditor from "@/components/review-editor";
-
+import Image from "next/image";
 // export const dynamicParams = false;
 export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
@@ -29,7 +29,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <Image
+          src={coverImgUrl}
+          width={240}
+          height={300}
+          alt={`도서 ${title}의 표지 이미지`}
+        />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
@@ -44,7 +49,7 @@ async function BookDetail({ bookId }: { bookId: string }) {
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
-    { next : {tags : [`review-${bookId}`]}}
+    { next: { tags: [`review-${bookId}`] } }
   );
 
   if (!response.ok) {
