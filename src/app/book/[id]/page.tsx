b335bef140134a0c5c54f1288d069b8afce3fdba
Min-byeonghyun <qudgus5125@naver.com>
2025-09-30 18:23:05 +0900

    Optimization

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -5,10 +5,22 @@ import ReviewItem from "@/components/review-item";
 import ReviewEditor from "@/components/review-editor";
 import Image from "next/image";
 import { Metadata } from "next";
-import { title } from "process";
+
 // export const dynamicParams = false;
-export function generateStaticParams() {
-  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+export async function generateStaticParams() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const books: BookData[] = await response.json();
+
+  return books.map((book) => ({
+    id: book.id.toString(),
+  }));
 }
 
 async function BookDetail({ bookId }: { bookId: string }) {
